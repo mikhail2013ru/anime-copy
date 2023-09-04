ec260a51bb01b94eb8d21fc30f16e35b7761d23d
mikhail2013ru <mikhail2013ru@gmail.com>
2023-09-04 20:48:12 +0300

    1 день

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 const mode = process.env.NODE_ENV || 'development'
 const devMode = mode === 'development'
 const target = devMode ? 'web' : 'browserslist'
@@ -9,19 +11,27 @@ module.exports = {
     mode,
     target,
     devtool,
+    devServer: {
+        open: true,
+        hot: true,
+    },
     entry: [ 
-        // "font-awesome-webpack!./node_modules/font-awesome-webpack/font-awesome.config.js",
+        '@babel/polyfill',
         path.resolve(__dirname, 'src/js', 'main.js'),
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         filename: '[name].[contenthash].js',
+        // assetModuleFilename: 'assets/[name][ext]'
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src', 'index.html')
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        })
     ],
     module: {
         rules: [
@@ -31,16 +41,79 @@ module.exports = {
             },
 
             {
-                test: /\.s[ac]ss$/i,
+                test: /\.woff2?$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
+            },
+
+            {
+                test: /\.(jpe?g|png|webp|gif|svg)?$/i,
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                            progressive: true,
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                            enabled: false,
+                            },
+                            pngquant: {
+                            quality: [0.65, 0.90],
+                            speed: 4
+                            },
+                            gifsicle: {
+                            interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                            quality: 75
+                            }
+                        }
+                    }
+                ],
+                type: 'asset/resource',
+                // generator: {
+                //     filename: 'images/[name][ext]'
+                // }
+            },
+
+            {
+                test: /\.(c|sa|sc)ss$/i,
                 use: [
                   // Creates `style` nodes from JS strings
-                  "style-loader",
+                  devMode ? "style-loader" : MiniCssExtractPlugin.loader,                  
                   // Translates CSS into CommonJS
                   "css-loader",
+                  {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: [require('postcss-preset-env')],
+                        }
+                    }
+                  },
                   // Compiles Sass to CSS
                   "sass-loader",
                 ],
-              },
+            },
+
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
         ],
     },
 }
