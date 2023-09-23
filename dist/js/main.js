722b95dc5841b1fb54bb71ae0d0a20c25c897c09
mikhail2013ru <mikhail2013ru@gmail.com>
2023-09-23 19:34:21 +0300

    3 день

--- dist/js/main.js
+++ dist/js/main.js
@@ -2651,7 +2651,6 @@ const bgElements = () => {
     elements.forEach(item => {
         const src = item.dataset.setbg
         item.style.backgroundImage = `url(${src})`
-        // console.log(src)    
     })
 }
 
@@ -2671,12 +2670,45 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 const mainData = () => {
+    const renderAnimeList = (array, ganres) => {
+        // console.log(array, ganres);
+    }
+
+    const renderTopAnime = (array) => {
+        const wrapper = document.querySelector('.filter__gallery')
+
+        wrapper.innerHTML = ''
+        
+        array.forEach(item => {
+            console.log(item);
+            wrapper.insertAdjacentHTML('afterbegin', `
+                <div class="product__sidebar__view__item set-bg mix"
+                    data-setbg="${item.image}">
+                    <div class="ep">${item.rating} / ?</div>
+                    <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+                    <h5><a href="/anime-details.html">${item.title}</a></h5>
+                </div>
+            `)
+        });
+
+        wrapper.querySelectorAll('.set-bg').forEach(item => {
+            const src = item.dataset.setbg
+            item.style.backgroundImage = `url(${src})`
+        })
+    }
+
     fetch('https://anime-b58d4-default-rtdb.firebaseio.com/anime.json')
         .then((response) => {
             return response.json()
         })
         .then((data) => {
-            console.log(data);
+            const ganres = new Set()
+            renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
+            data.forEach((item) => {
+                ganres.add(item.ganre)
+            })
+
+            renderAnimeList(data, ganres)
         })
 }
 
--- src/js/modal.js
+++ src/js/modal.js
@@ -2,13 +2,47 @@ const modal = () => {
     const modal = document.querySelector('.search-model')
     const modalBtn = document.querySelector('.icon_search')
     const modalClose = document.querySelector('.search-close-switch')
-    
+    const searchInput = document.querySelector('#search-input')
+    const wrapper = document.querySelector('.search-model-result')
+
+    wrapper.style.width = '100%'
+    wrapper.style.maxWidth = '500px'
+
+    const renderFunc = (items) => {
+        wrapper.innerHTML = ''
+
+        items.forEach(item => {
+            wrapper.insertAdjacentHTML('afterbegin', `
+                <a class="p-2" href="/anime-details.html">${item.title}</a>
+            `)
+        });
+    }
+
+    const searchFunc = (searchStr) => {
+        fetch('https://anime-b58d4-default-rtdb.firebaseio.com/anime.json')
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                const filteredData = data.filter(dataItem => {
+                    return dataItem.title.toLowerCase().includes(searchStr.toLowerCase()) || dataItem.description.toLowerCase().includes(searchStr.toLowerCase())
+                })
+                
+                const result = filteredData.slice(0, 5)    
+                renderFunc(result)
+            })
+    }
+
     modalBtn.addEventListener('click', () => {
         modal.classList.add('search-model--active')
     })
-    
+
     modalClose.addEventListener('click', () => {
         modal.classList.remove('search-model--active')
+    })
+
+    searchInput.addEventListener('input', (e) => {
+        searchFunc(e.target.value)
     })
 }
 
